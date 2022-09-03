66f48f3272acbeb19b6abf6bef9c57ed0d444371
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-03 21:42:46 +0300

    решена задача посчитать количество знаков в числе

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Введите число A ");
+int A = int.Parse(Console.ReadLine());
+// string a = Convert.ToString(A); // первый способ
+// Console.WriteLine(a.Length);
+
+int count = 0;
+A = Math.Abs(A);
+while(A !=0)
+{
+    A=A/10;
+    count++;
+}
+Console.WriteLine(count);
