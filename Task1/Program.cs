2fda25166d2781020e0c73590438927750cdfe7f
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-03 21:42:04 +0300

    решена задача факториал А

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//напишите программу, которая принимает на вход число А 
+//и выдает сумму чисел от 1 до А
+// 7=>28, 4=>10
+
+
+Console.Clear();
+Console.WriteLine("Введите число A ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSum(A));
+int GetSum(int A)
+{
+    int sum = 0;
+    for(int i = 0; i<=A; i++)
+    {
+        sum+=i; //sum = sum+i
+    }
+    return sum;
+}
