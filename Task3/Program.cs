a2c131088719af994eead281c3ba5c36491c28bd
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-03 21:53:02 +0300

    решена задача с произведением чисел от 1 до N

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+Console.Clear();
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= n; i++)
+{
+    result*=i;
+}
+Console.WriteLine(result);
