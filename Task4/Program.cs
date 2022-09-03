3b5077c72ba2a3417e5d505b365bfaa791503fb2
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-03 22:30:48 +0300

    решена задача вывод массива 8 чисел из 1 и 0

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+int [] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i] + " ");
+}
